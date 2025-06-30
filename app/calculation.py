056f7b1ef56a17197439a0f8024b51ae40a56e6e
Nahom2162002 <NahomAshagrea2002@gmail.com>
2025-06-30 17:18:30 -0400

    New calculations

--- app/calculation.py
+++ app/calculation.py
@@ -17,5 +17,21 @@ class Divide:
         return x / y 
 
 class Root:
-    def root(x):
-        math.sqrt(x) 
+    def root(x, y):
+        return math.pow(x, 1 / y) 
+
+class Power:
+    def power(x, y):
+        return math.pow(x, y)
+
+class Modulus:
+    def remainder(x, y):
+        return x % y 
+
+class Percentage:
+    def percentage(x, y):
+        return (x / y) * 100
+
+class AbsoluteDiff:
+    def absolute_diff(x, y):
+        return abs(x - y)
